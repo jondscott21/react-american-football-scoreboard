c1504807a24745ac2e285dc1c31b8a123ca85d1f
Jonathan Scott <jonathan.scott@gmail.com>
2019-07-15 15:11:32 -0700

    added a click handler for scoring buttons

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import Buttons from "./Buttons";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-const [homeScore, setHomeScore] = useState(0);
-const [awayScore, setAwayScore] = useState(0);
-const [quarter, setQuarter] = useState(1);
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
+  const [quarter, setQuarter] = useState(1);
+  const[homeTeam, setHomeTeam] = useState('Lions')
+  const[awayTeam, setAwayTeam] = useState('Tigers')
   return (
     <div className="container">
       <section className="scoreboard">
-        <TopRow homeScore={homeScore} awayScore={awayScore} />
+        <TopRow homeScore={homeScore} awayScore={awayScore} homeTeam={homeTeam} awayTeam={awayTeam} />
         <BottomRow quarter={quarter} />
       </section>
-      <Buttons homeScore={homeScore} setHomeScore={setHomeScore} awayScore={awayScore} setAwayScore={setAwayScore} quarter={quarter} setQuarter={setQuarter} />
+      <Buttons homeTeam={homeTeam} awayTeam={awayTeam} homeScore={homeScore} setHomeScore={setHomeScore} awayScore={awayScore} setAwayScore={setAwayScore} quarter={quarter} setQuarter={setQuarter} />
     </div>
   );
 }
--- src/Buttons.js
+++ src/Buttons.js
@@ -2,20 +2,28 @@ import React from "react";
 import "./App.css";
 
 function Buttons(props) {
+    const buttonClick = (team, score) => {
+        if (team === props.awayTeam) {
+            props.setAwayScore(props.awayScore + score);
+        } else {
+            props.setHomeScore(props.homeScore + score);
+        }
+    }
     return (
         <section className="buttons">
-            <button onClick={ () => props.setQuarter(props.quarter === 4 ? props.quarter - 3 : props.quarter + 1)} className="quarter-button">Quarter</button>
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={ () => props.setHomeScore(props.homeScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick={ () => props.setHomeScore(props.homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button onClick={ () => props.setAwayScore(props.awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={ () => props.setAwayScore(props.awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
-    ); 
+            
+            <div className="homeButtons">
+                {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+                <button dataset={props.homeTeam} onClick={() => buttonClick('Lions', 7)} className="homeButtons__touchdown">Home Touchdown</button>
+                <button dataset={props.homeTeam} onClick={() => buttonClick('Lions', 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+            </div>
+            <button onClick={() => props.setQuarter(props.quarter === 4 ? props.quarter - 3 : props.quarter + 1)} className="quarter-button">Quarter</button>
+            <div className="awayButtons">
+                <button dataset={props.awayTeam} onClick={() => buttonClick('Tigers', 7)} className="awayButtons__touchdown">Away Touchdown</button>
+                <button dataset={props.awayTeam} onClick={() => buttonClick('Tigers', 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
+            </div>
+        </section>
+    );
 }
 
 export default Buttons;
