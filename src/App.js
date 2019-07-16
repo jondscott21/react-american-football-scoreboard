ca158fd8b5f7084518bf16f53268fe40fb8a0cdd
Jonathan Scott <jonathan.scott@gmail.com>
2019-07-16 08:29:29 -0700

    fixed 'to go' and 'ball on' buttons

--- src/App.js
+++ src/App.js
@@ -10,15 +10,18 @@ function App() {
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
   const [quarter, setQuarter] = useState(1);
+  const [down, setDown] = useState(1);
+  const [toGo, setToGo] = useState(10);
+  const [ballOn, setBallOn] = useState(20);
   const[homeTeam, setHomeTeam] = useState('Lions')
   const[awayTeam, setAwayTeam] = useState('Tigers')
   return (
     <div className="container">
       <section className="scoreboard">
         <TopRow homeScore={homeScore} awayScore={awayScore} homeTeam={homeTeam} awayTeam={awayTeam} />
-        <BottomRow quarter={quarter} />
+        <BottomRow quarter={quarter} down={down} toGo={toGo}  ballOn={ballOn} />
       </section>
-      <Buttons homeTeam={homeTeam} awayTeam={awayTeam} homeScore={homeScore} setHomeScore={setHomeScore} awayScore={awayScore} setAwayScore={setAwayScore} quarter={quarter} setQuarter={setQuarter} />
+      <Buttons toGo={toGo} setToGo={setToGo} ballOn={ballOn} setBallOn={setBallOn} down={down} setDown={setDown} homeTeam={homeTeam} awayTeam={awayTeam} homeScore={homeScore} setHomeScore={setHomeScore} awayScore={awayScore} setAwayScore={setAwayScore} quarter={quarter} setQuarter={setQuarter} />
     </div>
   );
 }
